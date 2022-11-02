94db26cd1c0dcaa4f9d9b9f9a488995b0d2a9a7a
Inmyveinz <85662074+Inmyveinz@users.noreply.github.com>
2022-11-03 00:38:56 +0530

    Update start.sh
    Signed-off-by: Inmyveinz <85662074+Inmyveinz@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/0.git /0
+  git clone https://https://github.com/Inmyveinz/legend-rep-rok.git /0
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /0
